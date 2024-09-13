b6f4643eb2802f77c1b7b9d1919d7807472abde7
Difroy <unifroy@gmail.com>
2024-09-13 14:04:00 +0200

    Update AdministratorRepository.java

--- com/generation/clinic/repository/AdministratorRepository.java
+++ com/generation/clinic/repository/AdministratorRepository.java
@@ -1,7 +1,14 @@
 package com.generation.clinic.repository;
 
+import com.generation.clinic.model.entities.Administrator;
+
 public interface AdministratorRepository {
 
+	void insert (Administrator newAdministrator);
+	Administrator findById (int id);
+	Administrator findByEmailAndPassword (String email, String password);
+	void update (Administrator oldAdministrator);
+	void delete (int id);
 	
 	
 	
